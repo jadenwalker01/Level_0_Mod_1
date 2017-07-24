0e70ae4cad2dd25283a41237646db732fbf7db51
fredthefrenchtoast <30299526+fredthefrenchtoast@users.noreply.github.com>
2017-07-24 12:00:19 -0700

    Add files via upload

---
+++ src/for_loops/miilk.java
@@ -0,0 +1,14 @@
+package for_loops;
+
+public class miilk {
+	public static void main(String[] args) {
+	
+		for(int i = 0; i<99; i++){
+			
+			int m = 99-i;
+			System.out.println(m + " bottles of milk on the wall \n" + m +  " bottles of milk!\n" + "Take one down\nPass it around" + m + " bottles of milk");
+			
+		}
+		
+	}
+}
